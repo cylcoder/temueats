d4f85893aff80f807600efb82b77746e8793a059
cylcoder <cylcoder@gmail.com>
2024-11-07 14:50:00 +0900

    init: 프로젝트 초기 생성

---
+++ src/test/java/com/sparta/temueats/TemueatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.temueats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemueatsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
