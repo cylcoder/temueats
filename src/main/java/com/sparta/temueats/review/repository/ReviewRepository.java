c995faa3b8e2bedcd1bfa5e527b7532fc2b0e1bc
jeendale <jeekoo7203@gmail.com>
2024-11-13 07:33:56 +0900

    feat: 가게리뷰목록 조회

--- src/main/java/com/sparta/temueats/review/repository/ReviewRepository.java
+++ src/main/java/com/sparta/temueats/review/repository/ReviewRepository.java
@@ -3,13 +3,20 @@ package com.sparta.temueats.review.repository;
 import com.sparta.temueats.review.entity.P_review;
 import com.sparta.temueats.store.entity.P_store;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface ReviewRepository extends JpaRepository<P_review, UUID> {
-
-    P_store findByStore(Long storeId);
+    //가게 정보 가져오기
+    @Query("SELECT r.store FROM P_review r WHERE r.store.storeId = :storeId")
+    Optional<P_store> findStoreByStoreId(@Param("storeId") UUID storeId);
+    //가게UUID로 리뷰리스트 가져오기
+    @Query("SELECT r FROM P_review r WHERE r.store.storeId = :storeId")
+    List<P_review> findByStoreId(@Param("storeId") UUID storeId);
 
     List<P_review> findByUserId(Long userId);
 
