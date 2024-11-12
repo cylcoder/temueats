61ce3f6af0e60d271ba620bd7fe4e37426c5d41c
le-monaaa <kgbn013@naver.com>
2024-11-12 11:23:04 +0900

    feat: 즐겨찾기 관련 기능 구현
    - FavStoreRepository findByUser 쿼리 수정
    
    Resolves: #53

--- src/main/java/com/sparta/temueats/store/repository/FavStoreRepository.java
+++ src/main/java/com/sparta/temueats/store/repository/FavStoreRepository.java
@@ -5,14 +5,16 @@ import com.sparta.temueats.store.entity.P_store;
 import com.sparta.temueats.user.entity.P_user;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface FavStoreRepository extends JpaRepository<P_favStore, UUID> {
     Optional<P_favStore> findByUserAndStore(P_user user, P_store store);
 
-    @Query("SELECT f.store FROM P_FAV_STORE f WHERE f.user = :user ORDER BY f.created_at DESC")
+    @Query("SELECT f.store FROM P_FAV_STORE f WHERE f.user = :user ORDER BY f.createdAt DESC")
     List<P_store> findByUser(P_user user);
 }
