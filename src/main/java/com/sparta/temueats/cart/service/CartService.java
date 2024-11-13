e878acabe3d0beba785dedf8b1739e456f50428e
drinkgalaxy <drinkgalaxy1226@gmail.com>
2024-11-13 16:45:35 +0900

    fix: Customer, Owner 권한 추가 및 서비스 분리

--- src/main/java/com/sparta/temueats/cart/service/CartService.java
+++ src/main/java/com/sparta/temueats/cart/service/CartService.java
@@ -6,11 +6,8 @@ import com.sparta.temueats.cart.dto.CartGetListResponseDto;
 import com.sparta.temueats.cart.entity.P_cart;
 import com.sparta.temueats.cart.repository.CartRepository;
 import com.sparta.temueats.global.ex.CustomApiException;
-import com.sparta.temueats.menu.entity.Category;
 import com.sparta.temueats.menu.entity.P_menu;
 import com.sparta.temueats.menu.repository.MenuRepository;
-import com.sparta.temueats.store.entity.P_store;
-import com.sparta.temueats.store.entity.SellState;
 import com.sparta.temueats.user.entity.P_user;
 import com.sparta.temueats.user.entity.UserRoleEnum;
 import lombok.RequiredArgsConstructor;
@@ -22,57 +19,25 @@ import org.springframework.transaction.annotation.Transactional;
 import java.sql.Date;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class CartService {
-    // todo 더미 데이터 싹 수정
-
-    static GeometryFactory geometryFactory = new GeometryFactory();
-    static P_user staticCustomerUser = P_user.builder()
-            .id(4L)
-            .email("email@naver.com")
-            .password("임시1234")
-            .phone("010-1234-5678")
-            .nickname("네임")
-            .birth(Date.valueOf("2002-12-26"))
-            .use_yn(true)
-            .role(UserRoleEnum.OWNER)
-            .imageProfile("img_url")
-            .latLng(geometryFactory.createPoint(new Coordinate(123, 123)))
-            .address("123층 123호")
-            .build();
-
-    static P_user staticOwnerUser = P_user.builder()
-            .id(3L)
-            .email("email5@naver.com")
-            .password("임시12345")
-            .phone("010-5555-5555")
-            .nickname("네임5")
-            .birth(Date.valueOf("2002-12-26"))
-            .use_yn(true)
-            .role(UserRoleEnum.OWNER)
-            .imageProfile("img_url")
-            .latLng(geometryFactory.createPoint(new Coordinate(55, 55)))
-            .address("555층 555호")
-            .build();
-    static P_user usedUser = staticOwnerUser;
 
     private final CartRepository cartRepository;
     private final MenuRepository menuRepository;
 
-    public CartUpdateResponseDto createCarts(CartUpdateRequestDto cartUpdateRequestDto, Long user, UUID menuId) {
+    public CartUpdateResponseDto createCarts(CartUpdateRequestDto cartUpdateRequestDto, P_user user, UUID menuId) {
         // 1. 장바구니가 비어 있으면 바로 담기 & 바로 리턴
-        List<P_cart> allByUserId = cartRepository.findAllByUserId(usedUser.getId());
+        List<P_cart> allByUserId = cartRepository.findAllByUserId(user.getId());
         P_menu menu = menuRepository.findById(menuId).orElseThrow(() ->
                 new CustomApiException("해당 메뉴가 존재하지 않습니다."));
         if (allByUserId.isEmpty()) {
             P_cart cart3 = cartRepository.save(P_cart.builder()
                     .quantity(cartUpdateRequestDto.getQuantity())
                     .selectYn(false)
-                    .user(usedUser)
+                    .user(user)
                     .menu(menu)
                     .deletedYn(false)
                     .build());
@@ -80,7 +45,7 @@ public class CartService {
         }
 
         // 2-1. 장바구니에 같은 메뉴 id가 있는 경우
-        P_cart cart = cartRepository.findByMenuIdByUserId(menu.getMenuId(), usedUser.getId());
+        P_cart cart = cartRepository.findByMenuIdByUserId(menu.getMenuId(), user.getId());
         if (cart != null) {
             // 이미 같은 메뉴가 있다는 예외
             throw new CustomApiException("이미 해당 메뉴가 장바구니에 존재합니다.");
@@ -97,15 +62,15 @@ public class CartService {
         P_cart cart3 = cartRepository.save(P_cart.builder()
                         .quantity(cartUpdateRequestDto.getQuantity())
                         .selectYn(false)
-                        .user(usedUser)
+                        .user(user)
                         .menu(menu)
                         .deletedYn(false)
                 .build());
         return new CartUpdateResponseDto(cart3);
     }
 
-    public List<CartGetListResponseDto> getCarts() {
-        List<P_cart> cartList = cartRepository.findAllByUserId(usedUser.getId());
+    public List<CartGetListResponseDto> getCarts(P_user user) {
+        List<P_cart> cartList = cartRepository.findAllByUserId(user.getId());
         List<CartGetListResponseDto> responseDtoList = new ArrayList<>();
 
         for (P_cart cart : cartList) {
@@ -116,7 +81,7 @@ public class CartService {
     }
 
     @Transactional
-    public CartUpdateResponseDto updateCarts(CartUpdateRequestDto cartUpdateRequestDto, Long user, UUID cartId) {
+    public CartUpdateResponseDto updateCarts(CartUpdateRequestDto cartUpdateRequestDto, UUID cartId) {
         Long updatePrice = cartUpdateRequestDto.getQuantity();
 
         if (updatePrice <= 0 || updatePrice > 50) {
@@ -131,14 +96,14 @@ public class CartService {
     }
 
     @Transactional
-    public void deleteCarts(Long user, UUID cartId) {
+    public void deleteCarts(UUID cartId) {
         P_cart cart = cartRepository.findById(cartId).orElseThrow(() ->
                 new CustomApiException("해당 장바구니 품목을 찾을 수 없습니다."));
         cart.delete();
     }
 
     @Transactional
-    public void selectCarts(Long user, UUID cartId) {
+    public void selectCarts(UUID cartId) {
         P_cart cart = cartRepository.findById(cartId).orElseThrow(() ->
                 new CustomApiException("해당 장바구니 품목을 찾을 수 없습니다."));
         cart.changeSelect();
--- src/main/java/com/sparta/temueats/order/controller/OrderController.java
+++ src/main/java/com/sparta/temueats/order/controller/OrderController.java
@@ -4,9 +4,14 @@ import com.sparta.temueats.global.ResponseDto;
 import com.sparta.temueats.order.dto.DeliveryOrderCreateRequestDto;
 import com.sparta.temueats.order.dto.OrderGetResponseDto;
 import com.sparta.temueats.order.dto.TakeOutOrderCreateRequestDto;
+import com.sparta.temueats.order.service.OrderCustomerService;
+import com.sparta.temueats.order.service.OrderOwnerService;
 import com.sparta.temueats.order.service.OrderService;
+import com.sparta.temueats.security.UserDetailsImpl;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,42 +23,49 @@ import java.util.UUID;
 @RequestMapping("/api")
 public class OrderController {
 
-    static final Long USER_ID = 1L;
-
     private final OrderService orderService;
+    private final OrderCustomerService orderCustomerService;
+    private final OrderOwnerService orderOwnerService;
 
     // todo customer 접근 가능 권한 설정
     // todo owner 접근 가능 권한 설정
 
     // 주문 생성 (Customer)
+    @PreAuthorize("hasAnyAuthority('CUSTOMER', 'MASTER')")
     @PostMapping("/orders/customer")
-    public ResponseDto<?> orderCreateResponseCustomerDto(@RequestBody @Valid DeliveryOrderCreateRequestDto deliveryOrderCreateRequestDto, BindingResult bindingResult) {
-        orderService.createDeliveryOrders(deliveryOrderCreateRequestDto, USER_ID);
+    public ResponseDto<?> orderCreateResponseCustomerDto(@RequestBody @Valid DeliveryOrderCreateRequestDto deliveryOrderCreateRequestDto, BindingResult bindingResult,
+                                                         @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        orderCustomerService.createDeliveryOrders(deliveryOrderCreateRequestDto, userDetails.getUser());
         return new ResponseDto<>(1, "주문이 정상적으로 생성되었습니다.", null);
     }
 
     // 주문 생성 (Owner)
+    @PreAuthorize("hasAnyAuthority('OWNER', 'MASTER')")
     @PostMapping("/orders/owner")
-    public ResponseDto<?> orderCreateResponseOwnerDto(@RequestBody @Valid TakeOutOrderCreateRequestDto takeOutOrderCreateRequestDto, BindingResult bindingResult) {
-        orderService.createTakeOutOrders(takeOutOrderCreateRequestDto, USER_ID);
+    public ResponseDto<?> orderCreateResponseOwnerDto(@RequestBody @Valid TakeOutOrderCreateRequestDto takeOutOrderCreateRequestDto, BindingResult bindingResult,
+                                                      @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        orderOwnerService.createTakeOutOrders(takeOutOrderCreateRequestDto, userDetails.getUser());
         return new ResponseDto<>(1, "주문이 정상적으로 생성되었습니다.", null);
     }
 
     // 주문 목록 조회 (Customer)
+    @PreAuthorize("hasAnyAuthority('CUSTOMER', 'MASTER')")
      @GetMapping("/orders/customer")
-    public ResponseDto<?> orderGetCustomerResponseListDto() {
-         List<OrderGetResponseDto> customerOrders = orderService.getCustomerOrders();
+    public ResponseDto<?> orderGetCustomerResponseListDto(@AuthenticationPrincipal UserDetailsImpl userDetails) {
+         List<OrderGetResponseDto> customerOrders = orderCustomerService.getCustomerOrders(userDetails.getUser());
          return new ResponseDto<>(1, "주문 목록 조회에 성공했습니다.", customerOrders);
      }
 
     // 주문 목록 조회 (Owner)
+    @PreAuthorize("hasAnyAuthority('OWNER', 'MASTER')")
      @GetMapping("/orders/owner")
-    public ResponseDto<?> orderGetOwnerResponseListDto() {
-        List<OrderGetResponseDto> ownerOrders = orderService.getOwnerOrders();
+    public ResponseDto<?> orderGetOwnerResponseListDto(@AuthenticationPrincipal UserDetailsImpl userDetails) {
+        List<OrderGetResponseDto> ownerOrders = orderOwnerService.getOwnerOrders(userDetails.getUser());
         return new ResponseDto<>(1, "주문 목록 조회에 성공했습니다.", ownerOrders);
      }
 
     // 주문 상세 조회 (Customer)
+    @PreAuthorize("hasAnyAuthority('CUSTOMER', 'MASTER')")
     @GetMapping("/orders/customer/{orderId}")
     public ResponseDto<?> orderGetCustomerResponseDto(@PathVariable UUID orderId) {
         OrderGetResponseDto customerOrder = orderService.getOrder(orderId);
@@ -61,6 +73,7 @@ public class OrderController {
     }
 
     // 주문 상세 조회 (Owner)
+    @PreAuthorize("hasAnyAuthority('OWNER', 'MASTER')")
     @GetMapping("/orders/owner/{orderId}")
     public ResponseDto<?> orderGetOwnerResponseDto(@PathVariable UUID orderId) {
         OrderGetResponseDto ownerOrder = orderService.getOrder(orderId);
@@ -69,16 +82,18 @@ public class OrderController {
 
 
     // 주문 취소 (Customer)
+    @PreAuthorize("hasAnyAuthority('CUSTOMER', 'MASTER')")
     @PutMapping("/orders/{orderId}/customer")
     public ResponseDto<?> orderCancelCustomerResponseDto(@PathVariable UUID orderId) {
-        orderService.cancelCustomerOrder(orderId);
+        orderCustomerService.cancelCustomerOrder(orderId);
         return new ResponseDto<>(1, "주문이 취소되었습니다.", null);
     }
 
     // 주문 취소 (Owner)
+    @PreAuthorize("hasAnyAuthority('OWNER', 'MASTER')")
     @PutMapping("/orders/{orderId}/owner")
     public ResponseDto<?> orderCancelOwnerResponseDto(@PathVariable UUID orderId) {
-        orderService.cancelOwnerOrder(orderId);
+        orderOwnerService.cancelOwnerOrder(orderId);
         return new ResponseDto<>(1, "주문이 취소되었습니다.", null);
     }
 
