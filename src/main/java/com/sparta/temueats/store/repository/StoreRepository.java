e27881589cb90886b4242e4b96d13fedebcf2202
cylcoder <cylcoder@gmail.com>
2024-11-14 00:10:21 +0900

    chore: delete unused method

--- src/main/java/com/sparta/temueats/store/repository/StoreRepository.java
+++ src/main/java/com/sparta/temueats/store/repository/StoreRepository.java
@@ -14,29 +14,6 @@ public interface StoreRepository extends JpaRepository<P_store, UUID> {
 
     List<P_store> findByName(String name);
 
-   @Query("SELECT new com.sparta.temueats.store.dto.StoreResDto(s, " +
-           "COALESCE(AVG(r.score), 0), " +
-           "CAST(COUNT(rv) AS INTEGER), " +
-           "(CASE WHEN EXISTS (SELECT 1 FROM P_FAV_STORE f WHERE f.user.id = :userId AND f.store = s) THEN true ELSE false END)) " +
-           "FROM P_STORE s " +
-           "LEFT JOIN P_review rv ON rv.store = s " +
-           "LEFT JOIN P_RATING r ON r.store = s " +
-           "WHERE s.name LIKE %:storeName% " +
-           "GROUP BY s")
-    List<StoreResDto> findByStoreNameContaining(String storeName, Long userId);
-
-    @Query("SELECT new com.sparta.temueats.store.dto.StoreResDto(s, " +
-            "COALESCE(AVG(r.score), 0), " +
-            "CAST(COUNT(rv) AS INTEGER), " +
-            "(CASE WHEN EXISTS (SELECT 1 FROM P_FAV_STORE f WHERE f.user.id = :userId AND f.store = s) THEN true ELSE false END)) " +
-            "FROM P_STORE s " +
-            "LEFT JOIN P_review rv ON rv.store = s " +
-            "LEFT JOIN P_RATING r ON r.store = s " +
-            "LEFT JOIN P_MENU m ON m.store = s " +
-            "WHERE m.name LIKE %:menuName% " +
-            "GROUP BY s")
-    List<StoreResDto> findByMenuNameContaining(String menuName, Long userId);
-
     @Query("SELECT new com.sparta.temueats.store.dto.StoreResDto(s, " +
             "COALESCE(AVG(r.score), 0), " +
             "CAST(COUNT(rv) AS INTEGER), " +
