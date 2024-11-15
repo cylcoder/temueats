f20b2ad7de24e7c6c0447c20e110855bbea4d9f9
drinkgalaxy <drinkgalaxy1226@gmail.com>
2024-11-15 10:47:11 +0900

    fix: 주문 생성 기능 예외 처리 추가

--- src/main/java/com/sparta/temueats/cart/service/CartService.java
+++ src/main/java/com/sparta/temueats/cart/service/CartService.java
@@ -44,6 +44,8 @@ public class CartService {
             return new CartUpdateResponseDto(cart3);
         }
 
+        // + 가게가
+
         // 2-1. 장바구니에 같은 메뉴 id가 있는 경우
         P_cart cart = cartRepository.findByMenuIdByUserId(menu.getMenuId(), user.getId());
         if (cart != null) {
