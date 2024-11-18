5bf661f33d96491b8adf7f4b65038c7c520bad56
cylcoder <cylcoder@gmail.com>
2024-11-18 11:58:06 +0900

    chore: API 관련 코드 주석 해제

--- src/main/java/com/sparta/temueats/s3/config/S3Config.java
+++ src/main/java/com/sparta/temueats/s3/config/S3Config.java
@@ -4,13 +4,14 @@ import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class S3Config {
 
-   /* @Value("${cloud.aws.credentials.access-key}")
+    @Value("${cloud.aws.credentials.access-key}")
     private String accessKey;
 
     @Value("${cloud.aws.credentials.secret-key}")
@@ -25,16 +26,6 @@ public class S3Config {
 
         return (AmazonS3Client)AmazonS3ClientBuilder.standard()
                 .withRegion(region)
-                .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
-                .build();
-    }*/
-
-    @Bean
-    public AmazonS3Client amazonS3Client() {
-        BasicAWSCredentials awsCredentials = new BasicAWSCredentials("foo", "bar");
-
-        return (AmazonS3Client)AmazonS3ClientBuilder.standard()
-                .withRegion("baz")
                 .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
                 .build();
     }
